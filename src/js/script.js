47d7856a653077b2b4820aa21faab35b7a08935f
koko-soft <office.wkdg@gmail.com>
2023-08-19 01:53:02 +0900

    header, hamburger：完了、 drawer：未完成

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+	//Drawer
+	$(".js-hamburger,.sp-nav__items,.sp-nav__item a").click(function () {
+
+		if ($(".js-hamburger").hasClass('is-active')) {
+			$('.js-hamburger').removeClass("is-active");
+			$('.js-sp-nav').removeClass("is-active");
+		} else {
+			$('.js-hamburger').addClass("is-active");
+			$('.js-sp-nav').addClass("is-active");
+		};
+
+	});
 
 });
