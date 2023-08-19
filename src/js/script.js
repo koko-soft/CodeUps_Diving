c9355145bd06dc3433315068c03b015ea2e52bdb
koko-soft <office.wkdg@gmail.com>
2023-08-19 10:56:05 +0900

    MV：完了、campaign：未完成

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,88 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 	});
 
+	//Swiper --- mv
+	var swiper = new Swiper(".js-mv__swiper", {
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: 'true',
+		},
+		loop: 'true',
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		autoplay: {
+			delay: 4000,
+		},
+		speed: 1000,
+	});
+
+
+	//Swiper --- campaign
+	// var swiper = new Swiper(".js-campaign__swiper", {
+	// 	// loop: "true",
+	// 	slidesPerView: "auto",
+	// 	navigation: {
+	// 		nextEl: ".campaign__button-next",
+	// 		prevEl: ".campaign__button-prev",
+	// 	},
+	// 	autoplay: {
+	// 		delay: 2000,
+	// 	},
+	// 	speed: 500,
+
+	// 	// loop: true, // ループ有効
+	// 	// slidesPerView: auto, // 一度に表示する枚数
+	// 	// speed: 500, // ループの時間
+	// 	// allowTouchMove: false, // スワイプ無効
+	// 	// autoplay: {
+	// 	// 	delay: 0, // 途切れなくループ
+	// 	// },
+
+	// 	// loop: true,
+	// 	// // effect: "fade",
+	// 	// // speed: 3000,
+	// 	// allowTouchMove: false, //ユーザがスライドできるかどうか
+	// 	// autoplay: {
+	// 	// 	delay: 3000,
+	// 	// },
+	// 	// slidesPerView: auto, // 一度に表示する枚数
+	// 	// speed: 500, // ループの時間
+	// 	// allowTouchMove: false, // スワイプ無効
+	// 	// autoplay: {
+	// 	// 	delay: 3000, // 途切れなくループ
+	// 	// },
+	// });
+
+	// const swiper = new Swiper('.js-campaign__swiper', {
+	// 	direction: 'vertical',
+	// 	loop: true,
+	// 	autoplay: {
+	// 		delay: 2000,
+	// 	},
+	// 	slidesPerView: 'auto',
+	// 	speed: 500,
+	// 	navigation: {
+	// 		nextEl: '.swiper-button-next',
+	// 		prevEl: '.swiper-button-prev',
+	// 	},
+	// });
+
+	var swiper2 = new Swiper(".js-campaign__swiper", {
+		// effect: 'fade',
+		// fadeEffect: {
+		// 	crossFade: 'true',
+		// },
+		// loop: 'true',
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		autoplay: {
+			delay: 4000,
+		},
+		slidesPerView: '3',
+		speed: 1000,
+	});
 });
