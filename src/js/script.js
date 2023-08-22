e2c510a28839753f5cf4d8dfc52f43142c64d4b9
koko-soft <office.wkdg@gmail.com>
2023-08-22 14:44:58 +0900

    campaign(swiper)：完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
 
 	//Drawer
 	$(".js-hamburger,.sp-nav__items,.sp-nav__item a").click(function () {
@@ -20,82 +20,33 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 		fadeEffect: {
 			crossFade: 'true',
 		},
-		loop: 'true',
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
+		loop: true,
 		autoplay: {
 			delay: 4000,
 		},
 		speed: 1000,
 	});
-
 
 	//Swiper --- campaign
-	// var swiper = new Swiper(".js-campaign__swiper", {
-	// 	// loop: "true",
-	// 	slidesPerView: "auto",
-	// 	navigation: {
-	// 		nextEl: ".campaign__button-next",
-	// 		prevEl: ".campaign__button-prev",
-	// 	},
-	// 	autoplay: {
-	// 		delay: 2000,
-	// 	},
-	// 	speed: 500,
-
-	// 	// loop: true, // ループ有効
-	// 	// slidesPerView: auto, // 一度に表示する枚数
-	// 	// speed: 500, // ループの時間
-	// 	// allowTouchMove: false, // スワイプ無効
-	// 	// autoplay: {
-	// 	// 	delay: 0, // 途切れなくループ
-	// 	// },
-
-	// 	// loop: true,
-	// 	// // effect: "fade",
-	// 	// // speed: 3000,
-	// 	// allowTouchMove: false, //ユーザがスライドできるかどうか
-	// 	// autoplay: {
-	// 	// 	delay: 3000,
-	// 	// },
-	// 	// slidesPerView: auto, // 一度に表示する枚数
-	// 	// speed: 500, // ループの時間
-	// 	// allowTouchMove: false, // スワイプ無効
-	// 	// autoplay: {
-	// 	// 	delay: 3000, // 途切れなくループ
-	// 	// },
-	// });
-
-	// const swiper = new Swiper('.js-campaign__swiper', {
-	// 	direction: 'vertical',
-	// 	loop: true,
-	// 	autoplay: {
-	// 		delay: 2000,
-	// 	},
-	// 	slidesPerView: 'auto',
-	// 	speed: 500,
-	// 	navigation: {
-	// 		nextEl: '.swiper-button-next',
-	// 		prevEl: '.swiper-button-prev',
-	// 	},
-	// });
-
 	var swiper2 = new Swiper(".js-campaign__swiper", {
-		// effect: 'fade',
-		// fadeEffect: {
-		// 	crossFade: 'true',
-		// },
-		// loop: 'true',
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
+		loop: true,
 		autoplay: {
-			delay: 4000,
+			delay: 2000,
+			disableOnInteraction: false,
 		},
-		slidesPerView: '3',
 		speed: 1000,
+		navigation: {
+			nextEl: ".campaign__button-prev",
+			prevEl: ".campaign__button-next",
+		},
+		slidesPerView: 'auto',
+		spaceBetween: 24,
+		breakpoints: {
+			768: {
+				slidesPerView: 'auto',
+				spaceBetween: 40,
+			}
+		}
 	});
+
 });
