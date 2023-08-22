8f2a87c54958631c304d51c64ee2fab6b1254f7e
koko-soft <office.wkdg@gmail.com>
2023-08-23 00:51:41 +0900

    drawer：リンク、微調整

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 jQuery(function ($) {
 
 	//Drawer
-	$(".js-hamburger,.sp-nav__items,.sp-nav__item a").click(function () {
+	$(".js-hamburger,.js-sp-nav,.js-sp-nav a").click(function () {
 
 		if ($(".js-hamburger").hasClass('is-active')) {
 			$('.js-hamburger').removeClass("is-active");
@@ -12,6 +12,30 @@ jQuery(function ($) {
 			$('.js-sp-nav').addClass("is-active");
 		};
 
+	});
+
+	// Smooth Scroll
+	// #から始まるURLがクリックされた時
+	jQuery('a[href^="#"]').on('click', function () {
+
+		// headerの高さを取得
+		var header = jQuery('.header').innerHeight();
+		// hrefで指定されたidを取得
+		var id = jQuery(this).attr('href');
+		// 移動速度を指定（ミリ秒）
+		var speed = 500;
+		// idの値が#のみだったらターゲットをhtmlタグにする
+		var target = jQuery("#" == id ? "html" : id);
+		// ページのトップを基準にターゲットの位置を取得
+		var position = 0;
+		position = jQuery(target).offset().top - header;
+
+		jQuery('html, body').animate({
+			scrollTop: position
+		},
+			speed, 'swing');
+
+		return false;
 	});
 
 	//Swiper --- mv
