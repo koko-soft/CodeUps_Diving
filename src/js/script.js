da2d7c7fc79a05ad7bc13d1638105bea7dc8e93a
koko-soft <office.wkdg@gmail.com>
2023-08-25 08:55:57 +0900

    drawer,footer：修正

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,14 @@
 jQuery(function ($) {
 
 	//Drawer
-	$(".js-hamburger,.js-nav,.js-nav a").click(function () {
+	$(".js-hamburger,.js-sp-nav,.js-sp-nav a").click(function () {
 
 		if ($(".js-hamburger").hasClass('is-active')) {
 			$('.js-hamburger').removeClass("is-active");
-			$('.js-nav').removeClass("is-active");
+			$('.js-sp-nav').removeClass("is-active");
 		} else {
 			$('.js-hamburger').addClass("is-active");
-			$('.js-nav').addClass("is-active");
+			$('.js-sp-nav').addClass("is-active");
 		};
 
 	});
