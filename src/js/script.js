b4357d202b4a345a8d13f96e17b82c3cc30db9bb
koko-soft <office.wkdg@gmail.com>
2023-09-07 11:02:53 +0900

    fix：script.jsにinviewを追加

--- src/js/script.js
+++ src/js/script.js
@@ -125,7 +125,30 @@ jQuery(function ($) {
 		});
 	});
 
-	document.cookie = "cookie_name=cookie_value; expires=Sun, 01 Jan 2023 00:00:00 UTC; path=/; domain=codeups.com; secure; samesite=None";
+	// image-animation  inview
+	var box = $('.colorbox'),
+		speed = 700;
 
+	box.each(function () {
+		$(this).append('<div class="color"></div>')
+		var color = $(this).find($('.color')),
+			image = $(this).find('img');
+		var counter = 0;
+
+		image.css('opacity', '0');
+		color.css('width', '0%');
+		color.on('inview', function () {
+			if (counter == 0) {
+				$(this).delay(200).animate({ 'width': '100%' }, speed, function () {
+					image.css('opacity', '1');
+					$(this).css({ 'left': '0', 'right': 'auto' });
+					$(this).animate({ 'width': '0%' }, speed);
+				})
+				counter = 1;
+			}
+		});
+	});
+
+	document.cookie = "cookie_name=cookie_value; expires=Sun, 01 Jan 2023 00:00:00 UTC; path=/; domain=codeups.com; secure; samesite=None";
 
 });
