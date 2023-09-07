c0a41c2665285124349be3540a4b55be98c514fa
koko-soft <office.wkdg@gmail.com>
2023-09-07 10:20:36 +0900

    Revert "test：inviewテスト用"
    This reverts commit a3655b186ccf3e202a982167ece326b89f44ca25.

--- src/js/script.js
+++ src/js/script.js
@@ -128,31 +128,4 @@ jQuery(function ($) {
 	document.cookie = "cookie_name=cookie_value; expires=Sun, 01 Jan 2023 00:00:00 UTC; path=/; domain=codeups.com; secure; samesite=None";
 
 
-	//test
-	//要素の取得とスピードの設定
-	var box = $('.colorbox'),
-		speed = 700;
-
-	//.colorboxの付いた全ての要素に対して下記の処理を行う
-	box.each(function () {
-		$(this).append('<div class="color"></div>')
-		var color = $(this).find($('.color')),
-			image = $(this).find('img');
-		var counter = 0;
-
-		image.css('opacity', '0');
-		color.css('width', '0%');
-		//inviewを使って背景色が画面に現れたら処理をする
-		color.on('inview', function () {
-			if (counter == 0) {
-				$(this).delay(200).animate({ 'width': '100%' }, speed, function () {
-					image.css('opacity', '1');
-					$(this).css({ 'left': '0', 'right': 'auto' });
-					$(this).animate({ 'width': '0%' }, speed);
-				})
-				counter = 1;
-			}
-		});
-	});
-
 });
