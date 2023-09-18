38d098d1736c3961cfa51a2fb2bc518389b013ff
koko-soft <office.wkdg@gmail.com>
2023-09-19 01:02:29 +0900

    fix：loading中に裏がスクロールできないようにする

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -11,14 +11,21 @@ window.addEventListener('DOMContentLoaded', function () {
 	const loading = $(".js-loading");
 	const imgWrap = $(".loading__wrap");
 
-	tl1.set([left, right], { y: "100%", opacity: 1 })
+	tl1
+		.add(function () {
+			document.body.style.overflow = 'hidden';
+		})
+		.set([left, right], { y: "100%", opacity: 1 })
 		.set([titleWrap], { zIndex: 99999 })
-		.to([left], { duration: 2, y: "0%", ease: "power1.out", delay: 1 })
+		.to([left], { duration: 2, y: "0%", ease: "power1.out", delay: 0.6 })
 		.to([right], { duration: 2.2, y: "0%", ease: "power1.out", delay: 0.2 }, '<')
 		.to([title], { opacity: 1, duration: 1.5, delay: 0.5 })
 		.fromTo([subtitle], { opacity: 0, y: "50%" }, { opacity: 1, y: 0, duration: 1 }, '-=0.2')
 		.to([loading], { autoAlpha: 0, duration: 2, delay: 1.8 })
 		.to([imgWrap], { scale: 1.2, duration: 2 }, '<')
 		.set([titleWrap], { zIndex: 10 })
+		.add(function () {
+			document.body.style.overflow = 'auto';
+		})
 		;
 });
