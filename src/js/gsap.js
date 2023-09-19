fc7053c6a010af032502eb3b9916f833be6ed520
koko-soft <office.wkdg@gmail.com>
2023-09-19 22:24:29 +0900

    fix：Loadingの再調整

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -16,12 +16,12 @@ window.addEventListener('DOMContentLoaded', function () {
 			document.body.style.overflow = 'hidden';
 		})
 		.set([left, right], { y: "100%", opacity: 1 })
-		.to([left], { duration: 0.8, y: "0%", ease: "Power0.easeNone" })
-		.to([right], { duration: 0.9, y: "0%", ease: "Power0.easeNone", delay: 0.1 }, '<')
-		.fromTo([title], { y: "50%", opacity: 0 }, { y: "0%", opacity: 1, duration: 0.3, delay: 0.2 })
-		.fromTo([subtitle], { y: "50%", opacity: 0 }, { y: "0%", opacity: 1, duration: 0.2 })
-		.to([loading], { duration: 0.8, autoAlpha: 0, delay: 0.5 })
-		.to([imgWrap], { duration: 0.8, scale: 1.2 }, '<')
+		.to([left], { duration: 1, y: "0%", ease: "Power0.easeNone" })
+		.to([right], { duration: 1, y: "0%", ease: "Power0.easeNone", delay: 0.1 }, '<')
+		.fromTo([title], { opacity: 0 }, { opacity: 1, duration: 0.6, delay: 0.2 })
+		.fromTo([subtitle], { opacity: 0 }, { opacity: 1, duration: 0.4, delay: 0.4 }, '<')
+		.to([loading], { duration: 1, autoAlpha: 0, delay: 0.6 })
+		.to([imgWrap], { duration: 1, scale: 1.2 }, '<')
 		.add(function () {
 			document.body.style.overflow = 'auto';
 		})
