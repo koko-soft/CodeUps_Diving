524e31cec45f3d32f9e08d000e27aa9401f0c221
koko-soft <office.wkdg@gmail.com>
2023-09-20 14:58:16 +0900

    temp：動作自体はできたが要修正

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,78 @@
+'use strict';
 
 jQuery(function ($) {
 
-	//Loading
 	document.body.style.overflow = 'hidden';
+
+	//Loading
+	window.addEventListener('load', function () {
+
+		console.log('start');
+
+		const tl1 = gsap.timeline();
+		const circle = $(".js-loading-circle");
+		const left = $(".js-loading-left");
+		const right = $(".js-loading-right");
+		const title = $(".js-loading-title");
+		const subtitle = $(".js-loading-subtitle");
+		const loading = $(".js-loading");
+		const imgWrap = $(".loading__wrap");
+
+		if (sessionStorage.getItem('visited')) {
+			//2回目以降
+			console.log('2回目以降');
+
+			tl1
+				.to([loading], { duration: 0.9, autoAlpha: 0, delay: 0.6 })
+				.add(function () {
+					document.body.style.overflow = 'auto';
+				})
+				;
+
+		} else {
+			//初回
+			console.log('初回');
+
+			sessionStorage.setItem('visited', 'true');
+
+			tl1
+				.add(function () {
+					// document.body.style.overflow = 'hidden';
+				})
+				.to([circle], { duration: 0.1, autoAlpha: 0 })
+				.to([title, subtitle], { duration: 0.2, opacity: 0 })
+				.set([left, right], { y: "100%", opacity: 1 })
+				.to([left], { duration: 0.9, y: "0%", ease: "Power0.easeNone" })
+				.to([right], { duration: 0.9, y: "0%", ease: "Power0.easeNone", delay: 0.1 }, '<')
+				.fromTo([title], { opacity: 0 }, { opacity: 1, duration: 0.6, delay: 0.2 })
+				.fromTo([subtitle], { opacity: 0 }, { opacity: 1, duration: 0.4, delay: 0.4 }, '<')
+				.to([loading], { duration: 0.9, autoAlpha: 0, delay: 0.6 })
+				.to([imgWrap], { duration: 0.9, scale: 1.2 }, '<')
+				.add(function () {
+					document.body.style.overflow = 'auto';
+				})
+				;
+
+		}
+		// document.body.style.overflow = 'auto';
+
+
+	});
+
+
+	// checkVisited();
+
+	// function checkVisited() {
+	// 	if (sessionStorage.getItem('visited')) {
+	// 		//2回目以降
+
+	// 	} else {
+	// 		//初回
+	// 		sessionStorage.setItem('visited', 'true');
+	// 	}
+	// }
+	// })
+
 
 	//Drawer
 	$(".js-hamburger,.js-sp-nav,.js-sp-nav a").click(function () {
