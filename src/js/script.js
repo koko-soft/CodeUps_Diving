d4f4faa979fb34d1a098621830d57ed25e47b86c
koko-soft <office.wkdg@gmail.com>
2023-09-22 16:36:03 +0900

    update：Loadingアニメーションの導入

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,19 @@
 
 jQuery(function ($) {
 
-	document.body.style.overflow = 'hidden';
+	function isVisited() {
+		let bReturn = false;
 
-	//Loading
-	window.addEventListener('load', function () {
+		if (sessionStorage.getItem('visited')) {
+			bReturn = true
+		} else {
+			sessionStorage.setItem('visited', 'true');
+		}
 
-		console.log('start');
+		return bReturn;
+	}
+
+	function loadingAnimation() {
 
 		const tl1 = gsap.timeline();
 		const circle = $(".js-loading-circle");
@@ -18,44 +25,46 @@ jQuery(function ($) {
 		const loading = $(".js-loading");
 		const imgWrap = $(".loading__wrap");
 
-		if (sessionStorage.getItem('visited')) {
-			//2回目以降
-			console.log('2回目以降');
+		tl1
+			.to([circle], { duration: 0.1, autoAlpha: 0 })
+			.to([title, subtitle], { duration: 0.2, opacity: 0 })
+			.set([left, right], { y: "100%", opacity: 1 })
+			.to([left], { duration: 0.9, y: "0%", ease: "Power0.easeNone" })
+			.to([right], { duration: 0.9, y: "0%", ease: "Power0.easeNone", delay: 0.1 }, '<')
+			.fromTo([title], { opacity: 0 }, { opacity: 1, duration: 0.6, delay: 0.2 })
+			.fromTo([subtitle], { opacity: 0 }, { opacity: 1, duration: 0.4, delay: 0.4 }, '<')
+			.to([loading], { duration: 0.9, autoAlpha: 0, delay: 0.6 })
+			.to([imgWrap], { duration: 0.9, scale: 1.2 }, '<')
+			;
 
-			tl1
-				.to([loading], { duration: 0.9, autoAlpha: 0, delay: 0.6 })
-				.add(function () {
-					document.body.style.overflow = 'auto';
-				})
-				;
+	}
+
+	function loadingClose() {
+
+		const tl1 = gsap.timeline();
+		const loading = $(".js-loading");
+
+		tl1.to([loading], { duration: 0.9, autoAlpha: 0, delay: 0.6 })
+			;
+
+	}
+
+	//Loading
+	window.addEventListener('load', function () {
+
+		document.body.style.overflow = 'hidden';
+
+		if (!isVisited()) {
+
+			loadingAnimation();
 
 		} else {
-			//初回
-			console.log('初回');
 
-			sessionStorage.setItem('visited', 'true');
-
-			tl1
-				.add(function () {
-					// document.body.style.overflow = 'hidden';
-				})
-				.to([circle], { duration: 0.1, autoAlpha: 0 })
-				.to([title, subtitle], { duration: 0.2, opacity: 0 })
-				.set([left, right], { y: "100%", opacity: 1 })
-				.to([left], { duration: 0.9, y: "0%", ease: "Power0.easeNone" })
-				.to([right], { duration: 0.9, y: "0%", ease: "Power0.easeNone", delay: 0.1 }, '<')
-				.fromTo([title], { opacity: 0 }, { opacity: 1, duration: 0.6, delay: 0.2 })
-				.fromTo([subtitle], { opacity: 0 }, { opacity: 1, duration: 0.4, delay: 0.4 }, '<')
-				.to([loading], { duration: 0.9, autoAlpha: 0, delay: 0.6 })
-				.to([imgWrap], { duration: 0.9, scale: 1.2 }, '<')
-				.add(function () {
-					document.body.style.overflow = 'auto';
-				})
-				;
+			loadingClose();
 
 		}
-		// document.body.style.overflow = 'auto';
 
+		document.body.style.overflow = 'auto';
 
 	});
 
